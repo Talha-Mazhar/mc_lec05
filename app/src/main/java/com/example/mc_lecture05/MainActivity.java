021b8ddaeafbfe92ed9d2b5cb5756da7b2dda9d1
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-01-15 17:18:17 +0500

    implementsOnClickListener

--- app/src/main/java/com/example/mc_lecture05/MainActivity.java
+++ app/src/main/java/com/example/mc_lecture05/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView digi;
     Button btnI, btnD, btnR;
@@ -21,70 +21,36 @@ public class MainActivity extends AppCompatActivity {
         btnI = findViewById(R.id.buttonInc);
         btnD = findViewById(R.id.buttonDec);
         btnR = findViewById(R.id.buttonreset);
-//        btnI.setOnClickListener(this);
-//        btnD.setOnClickListener(this);
-//        btnR.setOnClickListener(this);
-//        digi.setOnClickListener(this);
-
-        digi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnI.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+        btnR.setOnClickListener(this);
+        digi.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textViewer:
                 String s = Integer.toString(value);
                 digi.setText(s);
-            }
-        });
-
-        btnI.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                value=value+1;
-                String  s = Integer.toString(value);
+                break;
+            case R.id.buttonInc:
+                value++;
+                s = Integer.toString(value);
                 digi.setText(s);
-            }
-        });
-
-        btnD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                value=value-1;
-                String s = Integer.toString(value);
+                break;
+            case R.id.buttonDec:
+                value--;
+                s = Integer.toString(value);
                 digi.setText(s);
-            }
-        });
-        btnR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.buttonreset:
                 value=0;
-                String s = Integer.toString(value);
+                s = Integer.toString(value);
                 digi.setText(s);
-            }
-        });
+                break;
+            default:
+                s = Integer.toString(value);
+                digi.setText(s);
+                break;
+        }
     }}
-//    @Override
-//    public void onClick(View view) {
-//        switch (view.getId()) {
-//            case R.id.textViewer:
-//                String s = Integer.toString(value);
-//                digi.setText(s);
-//                break;
-//            case R.id.buttonInc:
-//                value++;
-//                s = Integer.toString(value);
-//                digi.setText(s);
-//                break;
-//            case R.id.buttonDec:
-//                value--;
-//                s = Integer.toString(value);
-//                digi.setText(s);
-//                break;
-//            case R.id.buttonreset:
-//                value=0;
-//                s = Integer.toString(value);
-//                digi.setText(s);
-//                break;
-//            default:
-//                s = Integer.toString(value);
-//                digi.setText(s);
-//                break;
-//        }
-//    }
